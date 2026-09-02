e3718e19c05e45eee6e2cca010dc694e899f02b7
Tavotsu <123342265+Tavotsu@users.noreply.github.com>
2026-09-02 10:27:19 -0400

    chore: inicializar proyectos base de catalogo y compras con spring boot

---
+++ src/test/java/com/pedidos360/catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos360.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
